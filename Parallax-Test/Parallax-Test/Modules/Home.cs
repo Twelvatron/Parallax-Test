f6d5e8d800114d27b1032bcb3a91d1c5ad1df0a6
unknown <Twelve@Twelve-PC.(none)>
2012-02-26 11:43:10 +0000

    please

---
+++ Parallax-Test/Parallax-Test/Modules/Home.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nancy;
+
+namespace Parallax_Test.Modules
+{
+    public class Home : NancyModule
+    {
+        public Home()
+        {
+            Get["/"] = _ => "Parallax me";
+            Get["/{pname}"] = pname;
+        }
+
+        public Response pname(dynamic parameters)
+        {
+            string nameIn = parameters.pname;
+            char[] name = nameIn.ToCharArray();
+            return View["Name.cshtml", name];
+        }
+    }
+}
